533ab03138eac997a6b68dba03cdb986765f0710
azma2008 <azmakagdi20@gmail.com>
2026-06-04 08:38:09 +0530

    Add files via upload

---
+++ arithmatic expression.c
@@ -0,0 +1,19 @@
+//Name:AZMA KAGDI//
+//PRN 2503033111372003//
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Addition = %d\n", a + b);
+    printf("Subtraction = %d\n", a - b);
+    printf("Multiplication = %d\n", a * b);
+    printf("Division = %d\n", a / b);
+    printf("Modulus = %d\n", a % b);
+
+    return 0;
+}
