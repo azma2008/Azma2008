8b4b59935838d8c1eba5f03b3a3853c85322e196
azma2008 <azmakagdi20@gmail.com>
2026-06-04 19:07:27 +0530

    Add files via upload

---
+++ factorial number.c
@@ -0,0 +1,19 @@
+//Name:AZMA KAGDI//
+//PRN 2503033111372003//
+#include <stdio.h>
+int main()
+{
+    int i = 1, n, fact = 1;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    printf("Factorial: %d", fact);
+
+    return 0;
+}
