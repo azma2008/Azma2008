ef1e7649863c99dfe34fcc51addb00d431ac4d59
azma2008 <azmakagdi20@gmail.com>
2026-07-09 14:10:13 +0530

    Add files via upload

---
+++ nested loop 4 ( Right aligned lower triangle ).c
@@ -0,0 +1,23 @@
+//NAME: AZMA KAGDI//
+//PRN:2503033111372003//
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for(i = 3; i >= 1; i--) {
+
+        for(j = 1; j <= 3 - i; j++) {
+            printf(" ");
+        }
+
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
