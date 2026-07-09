efea6ca79c34fb77fc05f2db1a224a0c70298d0c
azma2008 <azmakagdi20@gmail.com>
2026-07-09 14:29:16 +0530

    Add files via upload

---
+++ Iterative loop using 7 number.c
@@ -0,0 +1,21 @@
+//Name: AZMA KAGDI//
+//PRN 2503033111372003//
+
+#include <stdio.h>
+
+int main()
+{
+    int i, table, num;
+
+    printf("Enter A Number: ");
+    scanf("%d", &num);
+
+    for(i = 1; i <= 10; i++)
+    {
+        table = i * num;
+
+        printf("%d\n", table);
+    }
+
+    return 0;
+}
