e989fb27058be48cbbaea4f76622f6b8a371bafe
azma2008 <azmakagdi20@gmail.com>
2026-07-09 14:13:40 +0530

    Add files via upload

---
+++ Nested loop (3×3 pattern).c
@@ -0,0 +1,21 @@
+//Name: AZMA KAGDI//
+//PRN: 2503033111372003//
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 1; i <= 3; i++)
+    {
+        for(j = 1; j <= 3; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
