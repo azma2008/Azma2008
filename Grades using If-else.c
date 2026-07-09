986fc9065b530c68cfc9d4cdf539316c9e69f9e4
azma2008 <azmakagdi20@gmail.com>
2026-07-09 14:32:48 +0530

    Add files via upload

---
+++ Grades using If-else.c
@@ -0,0 +1,26 @@
+//Name: AZMA KAGDI//
+//PRN 2503033111372003//
+
+#include <stdio.h>
+
+int main() {
+    int marks;
+
+    printf("Enter marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 75) {
+        printf("Grade A");
+    }
+    else if (marks >= 60) {
+        printf("Grade B");
+    }
+    else if (marks >= 40) {
+        printf("Grade C");
+    }
+    else {
+        printf("Grade F (Fail)");
+    }
+
+    return 0;
+}
