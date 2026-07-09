25fa379e8da1679dafc02be5d96af43d7bc048ed
azma2008 <azmakagdi20@gmail.com>
2026-07-09 15:06:44 +0530

    Add files via upload

---
+++ nth powe of function.c
@@ -0,0 +1,16 @@
+//Name:Azma Mubasshir Kagdi //
+//PRN:2503033111372003//
+#include <stdio.h>
+int main() {
+int num, p, i;
+int ans = 1;
+printf("Enter number: \t");
+scanf("%d", &num);
+printf("Enter power: \t");
+scanf("%d", &p);
+for(i = 1; i <= p; i++) {
+ans = ans * num;
+}
+printf("Answer is %d\n", ans);
+return 0;
+}
